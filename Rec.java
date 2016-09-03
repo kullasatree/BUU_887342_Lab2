b2f5231ba4da3221d1bcd14125821e99edfb9421
kullasatree <kullasatree-_-@hotmail.com>
2016-09-03 11:38:02 +0700

    Update Rec.java

--- Rec.java
+++ Rec.java
@@ -6,11 +6,15 @@ public class Rec {
 		numDigits(1356) = 4.
 		Precondition: n >= 0. */
     public static int numDigits(int n) {
+	
 	// fill me in
-	if(n<10) && (n>-10)) return 1 ;
-	return 1+numDigits(n/10);
+	
     }
 
+    /** = sum of the digits in the decimal representation of n.
+      e.g. sumDigits(0) = 0, sumDigits(3) = 3, sumDigits(34) = 7,
+      sumDigits(345) = 12.
+      Precondition: n >= 0. */
     public static int sumDigits(int n) {
 	
 	// fill me in
@@ -20,9 +24,11 @@ public class Rec {
     /** = a copy of s with to_remove_char removed.
 		Example: removeChar("abeabe", 'e') = "abab". */
 	public static String removeChar(String s, char to_remove_char) {
-	
-	// fill me in
-	
+	if (s.length() 0 == ) return"";
+		if (s.charAt(1) == to_remove_char) return removeChar(s.substring(1),
+		to_remove_char);
+		else return String.valueOf(s.charAt(1)) + removeChar(s.substring(1),
+		to_remove_char);
 	}
 	
     /** = a copy of s with characters in reverse order.
